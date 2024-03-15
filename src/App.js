241f55021e95a8e234b0629da17ba5daae21be72
KULBHUSHANSINGH-arch <75601832+KULBHUSHANSINGH-arch@users.noreply.github.com>
2024-03-15 18:19:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+
+import './index.css';
+import LoginSign from './Components/LoginSignUp/loginSign';
+
+function App() {
+  return (
+    <div >
+     <LoginSign />
+    </div>
+  );
+}
+
+export default App;
